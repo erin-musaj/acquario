0a1079ca727087b9e6b2725aededbf5455f1a0db
erin-musaj <erin.musaj@buonarroti.tn.it>
2023-10-14 17:21:50 +0200

    feat: Costruttore, getter e setter

---
+++ src/Pesce.java
@@ -0,0 +1,26 @@
+public class Pesce {
+    private String nome;
+    private String razza;
+
+    public Pesce(String nome, String razza) {
+        this.nome = nome;
+        this.razza = razza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazza() {
+        return razza;
+    }
+
+    public void setRazza(String razza) {
+        this.razza = razza;
+    }
+
+}
